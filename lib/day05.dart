f89ab168496b5dd3188213f9d828d626a3dd69cc
Julian König <julian.koenig@js-soft.com>
2023-11-14 13:51:15 +0100

    feat: day 5

---
+++ lib/day05.dart
@@ -0,0 +1,39 @@
+import 'utils/utils.dart';
+
+main(List<String> args) => runSolutions((i) => i.asString(), part1, part2);
+
+int part1(List<String> input) => input.where(isStringNiceP1).length;
+
+bool isStringNiceP1(String input) {
+  // It does not contain the strings ab, cd, pq, or xy, even if they are part of one of the other requirements.
+  if (input.contains("ab") ||
+      input.contains("cd") ||
+      input.contains("pq") ||
+      input.contains("xy")) {
+    return false;
+  }
+
+  // It contains at least three vowels (aeiou only), like aei, xazegov, or aeiouaeiouaeiou.
+  final vowels = RegExp(r"[aeiou].*[aeiou].*[aeiou]");
+  if (!vowels.hasMatch(input)) return false;
+
+  // It contains at least one letter that appears twice in a row, like xx, abcdde (dd), or aabbccdd (aa, bb, cc, or dd).
+  final doubleLetter = RegExp(r"([a-z])\1");
+  if (!doubleLetter.hasMatch(input)) return false;
+
+  return true;
+}
+
+int part2(List<String> input) => input.where(isStringNiceP2).length;
+
+bool isStringNiceP2(String input) {
+  // It contains a pair of any two letters that appears at least twice in the string without overlapping, like xyxy (xy) or aabcdefgaa (aa), but not like aaa (aa, but it overlaps).
+  final pair = RegExp(r"([a-z]{2}).*\1");
+  if (!pair.hasMatch(input)) return false;
+
+  // It contains at least one letter which repeats with exactly one letter between them, like xyx, abcdefeghi (efe), or even aaa.
+  final repeat = RegExp(r"([a-z]).\1");
+  if (!repeat.hasMatch(input)) return false;
+
+  return true;
+}
---
+++ test/day05.dart
@@ -0,0 +1,35 @@
+import 'package:aoc2015/day05.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group("part1", () {
+    final tests = [
+      ("ugknbfddgicrmopn", true),
+      ("aaa", true),
+      ("jchzalrnumimnmhp", false),
+      ("haegwjzuvuyypxyu", false),
+      ("dvszwmarrgswjxmb", false),
+    ];
+
+    tests.forEach((t) {
+      test('isStringNiceP1(${t.$1}) returns ${t.$2}', () {
+        expect(isStringNiceP1(t.$1), t.$2);
+      });
+    });
+  });
+
+  group("part2", () {
+    final tests = [
+      ("qjhvhtzxzqqjkmpb", true),
+      ("xxyxx", true),
+      ("uurcxstgmygtbstg", false),
+      ("ieodomkazucvgmuy", false),
+    ];
+
+    tests.forEach((t) {
+      test('isStringNiceP2(${t.$1}) returns ${t.$2}', () {
+        expect(isStringNiceP2(t.$1), t.$2);
+      });
+    });
+  });
+}
