f7d7d0f81e125124865cab86a1f0b16b04cc6a23
Julian König <julian.koenig@js-soft.com>
2023-11-14 15:40:37 +0100

    feat: day 8

---
+++ lib/day08.dart
@@ -0,0 +1,29 @@
+import 'utils/utils.dart';
+
+main(List<String> args) => runSolutions((i) => i.asString(), part1, part2);
+
+int part1(List<String> input) {
+  final codeLength = input.map((e) => e.length).sum;
+
+  final unescaped = input
+      .map(
+        (e) => e
+            .substring(1, e.length - 1)
+            .replaceAll(RegExp(r'\\(\\|"|x[0-9a-f]{2})'), "'"),
+      )
+      .toList();
+
+  final unescapedLength = unescaped.map((e) => e.length).sum;
+  return codeLength - unescapedLength;
+}
+
+int part2(List<String> input) {
+  final codeLength = input.map((e) => e.length).sum;
+
+  final escaped = input
+      .map((e) => e.replaceAll("\\", "\\\\").replaceAll("\"", "\\\""))
+      .toList();
+
+  final escapedLength = escaped.map((e) => e.length + 2).sum;
+  return escapedLength - codeLength;
+}
---
+++ test/day08.dart
@@ -0,0 +1,19 @@
+import 'package:aoc2015/day08.dart';
+import 'package:test/test.dart';
+
+void main() {
+  final testInput = [
+    '""',
+    '"abc"',
+    '"aaa\\"aaa"',
+    '"\\x27"',
+  ];
+
+  test('part 1', () {
+    expect(part1(testInput), 12);
+  });
+
+  test('part 2', () {
+    expect(part2(testInput), 19);
+  });
+}
