8e933d21589d25ca6aabe2d061ab8b39e91b5d7d
Julian König <julian.koenig@js-soft.com>
2023-11-15 08:20:24 +0100

    feat: day 10

---
+++ lib/day10.dart
@@ -0,0 +1,22 @@
+import 'utils/utils.dart';
+
+main(List<String> args) => runSolutions((i) => i.raw(), part1, part2);
+
+int part1(String input) => lookAndSayXTimes(input, 40).length;
+
+int part2(String input) => lookAndSayXTimes(input, 50).length;
+
+String lookAndSayXTimes(String input, int times) {
+  for (var i = 0; i < times; i++) {
+    input = lookAndSay(input);
+  }
+
+  return input;
+}
+
+final _sameDigitRegex = RegExp(r'(\d)\1*');
+
+String lookAndSay(String input) => input.replaceAllMapped(
+      _sameDigitRegex,
+      (match) => match.group(0)!.length.toString() + match.group(1)!,
+    );
---
+++ test/day10.dart
@@ -0,0 +1,13 @@
+import 'package:aoc2015/day10.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test("look and say once", () {
+    expect(lookAndSay("1111112223333333"), "613273");
+  });
+
+  test('look and say x times', () {
+    expect(lookAndSayXTimes("1", 5), "312211");
+    expect(lookAndSayXTimes("1", 10), "11131221133112132113212221");
+  });
+}
