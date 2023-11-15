94d8d7fbb9ff1fe2e815ef68778b6e930ccbce6f
Julian König <julian.koenig@js-soft.com>
2023-11-15 09:21:24 +0100

    feat: day 11

---
+++ lib/day11.dart
@@ -0,0 +1,40 @@
+import 'utils/utils.dart';
+
+main(List<String> args) => runSolutions((i) => i.raw(), part1, part2);
+
+String part1(String input) => increasePassword(input);
+
+String part2(String input) => increasePassword(increasePassword(input));
+
+String increasePassword(String password) {
+  do {
+    final chars = password.split('');
+    for (var i = chars.length - 1; i >= 0; i--) {
+      if (chars[i] == 'z') {
+        chars[i] = 'a';
+      } else {
+        chars[i] = String.fromCharCode(chars[i].codeUnitAt(0) + 1);
+        break;
+      }
+    }
+
+    password = chars.join('');
+  } while (!isPasswordValid(password));
+
+  return password;
+}
+
+bool isPasswordValid(String password) {
+  if (password.contains('i') ||
+      password.contains('o') ||
+      password.contains('l')) return false;
+
+  final twoNonOverlappingPairsRegex = RegExp(r'(\w)\1.*(\w)\2');
+  if (!twoNonOverlappingPairsRegex.hasMatch(password)) return false;
+
+  final increasingStraightRegex = RegExp(
+      r'abc|bcd|cde|def|efg|fgh|ghi|hij|ijk|jkl|klm|lmn|mno|nop|opq|pqr|qrs|rst|stu|tuv|uvw|vwx|wxy|xyz');
+  if (!increasingStraightRegex.hasMatch(password)) return false;
+
+  return true;
+}
---
+++ test/day11.dart
@@ -0,0 +1,19 @@
+import 'package:aoc2015/day11.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test("passwords match", () {
+    expect(isPasswordValid("hijklmmn"), false);
+    expect(isPasswordValid("abbceffg"), false);
+    expect(isPasswordValid("abbcegjk"), false);
+    expect(isPasswordValid("abcdefgh"), false);
+    expect(isPasswordValid("abcdffaa"), true);
+    expect(isPasswordValid("ghijklmn"), false);
+    expect(isPasswordValid("ghjaabcc"), true);
+  });
+
+  test("increase passwords", () {
+    expect(increasePassword("abcdefgh"), "abcdffaa");
+    expect(increasePassword("ghijklmn"), "ghjaabcc");
+  });
+}
