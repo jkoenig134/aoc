d01e38662d510fc86e31c0767f758274365a81c3
Julian König <julian.koenig@js-soft.com>
2023-11-16 21:04:51 +0100

    feat: day 17

---
+++ lib/day17.dart
@@ -0,0 +1,30 @@
+import 'package:trotter/trotter.dart';
+
+import 'utils/utils.dart';
+
+main(List<String> args) =>
+    runSolutions((i) => i.asInt().map(Container.new).toList(), part1, part2);
+
+class Container {
+  final int size;
+  Container(this.size);
+}
+
+int part1(List<Container> input) => fillEggnog(input, 150).length;
+
+int part2(List<Container> input) => numberOfEggnogWithMinContainers(input, 150);
+
+List<List<Container>> fillEggnog(List<Container> containers, int liters) {
+  final compounds = Subsets(containers)().toList();
+
+  return compounds
+      .where((c) => c.map((cnt) => cnt.size).sum == liters)
+      .toList();
+}
+
+int numberOfEggnogWithMinContainers(List<Container> containers, int liters) {
+  final possiblities = fillEggnog(containers, liters);
+
+  final minContainers = possiblities.map((c) => c.length).min;
+  return possiblities.where((c) => c.length == minContainers).length;
+}
---
+++ test/day17.dart
@@ -0,0 +1,31 @@
+import 'package:aoc2015/day17.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test("fill eggnog", () {
+    expect(
+      fillEggnog([
+        Container(20),
+        Container(15),
+        Container(10),
+        Container(5),
+        Container(5),
+      ], 25)
+          .length,
+      4,
+    );
+  });
+
+  test("number of eggnog with min containers", () {
+    expect(
+      numberOfEggnogWithMinContainers([
+        Container(20),
+        Container(15),
+        Container(10),
+        Container(5),
+        Container(5),
+      ], 25),
+      3,
+    );
+  });
+}
