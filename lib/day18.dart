64ab3a20971feaef92cca49ea1e643899010473f
Julian König <julian.koenig@js-soft.com>
2023-11-17 09:40:07 +0100

    feat: day 18

---
+++ lib/day18.dart
@@ -0,0 +1,75 @@
+import 'utils/utils.dart';
+
+main(List<String> args) => runSolutions((i) => i.asStringList(), part1, part2);
+
+int part1(List<List<String>> input) => stepXTimes(input, 100);
+int part2(List<List<String>> input) =>
+    stepXTimes(input, 100, keepCornersOn: true);
+
+int stepXTimes(
+  List<List<String>> input,
+  int times, {
+  bool keepCornersOn = false,
+}) {
+  var current = input;
+
+  for (int i = 0; i < times; i++) {
+    current = step(current, keepCornersOn: keepCornersOn);
+  }
+
+  return current.expand((e) => e).where((e) => e == "#").length;
+}
+
+List<List<String>> step(
+  List<List<String>> input, {
+  bool keepCornersOn = false,
+}) {
+  final gridSize = input.length;
+
+  if (keepCornersOn) {
+    input[0][0] = "#";
+    input[0][gridSize - 1] = "#";
+    input[gridSize - 1][0] = "#";
+    input[gridSize - 1][gridSize - 1] = "#";
+  }
+
+  final out = List.generate(gridSize, (_) => List.filled(gridSize, "."));
+
+  for (int x = 0; x < out.length; x++) {
+    for (int y = 0; y < out.length; y++) {
+      final neighbors = [
+        (x - 1, y - 1),
+        (x - 1, y),
+        (x - 1, y + 1),
+        (x, y - 1),
+        (x, y + 1),
+        (x + 1, y - 1),
+        (x + 1, y),
+        (x + 1, y + 1),
+      ].where(
+        (n) => n.$1 >= 0 && n.$2 >= 0 && n.$1 < gridSize && n.$2 < gridSize,
+      );
+
+      final currentState = input[x][y];
+      final onNeighbors =
+          neighbors.where((n) => input[n.$1][n.$2] == "#").length;
+
+      final nextState = switch ((currentState, onNeighbors)) {
+        ("#", 2) || ("#", 3) => "#",
+        (".", 3) => "#",
+        _ => ".",
+      };
+
+      out[x][y] = nextState;
+    }
+  }
+
+  if (keepCornersOn) {
+    out[0][0] = "#";
+    out[0][gridSize - 1] = "#";
+    out[gridSize - 1][0] = "#";
+    out[gridSize - 1][gridSize - 1] = "#";
+  }
+
+  return out;
+}
---
+++ test/day18.dart
@@ -0,0 +1,86 @@
+import 'package:aoc2015/day18.dart';
+import 'package:test/test.dart';
+
+void main() {
+  final initial = [
+    [".", "#", ".", "#", ".", "#"],
+    [".", ".", ".", "#", "#", "."],
+    ["#", ".", ".", ".", ".", "#"],
+    [".", ".", "#", ".", ".", "."],
+    ["#", ".", "#", ".", ".", "#"],
+    ["#", "#", "#", "#", ".", "."],
+  ];
+
+  test("one step", () {
+    final result = step(initial);
+    result.debugPrint();
+
+    final onLights = result.expand((e) => e).where((e) => e == "#").length;
+    expect(onLights, 11);
+  });
+
+  test("4 steps", () {
+    final result = step(step(step(step(initial))));
+    result.debugPrint();
+
+    final onLights = result.expand((e) => e).where((e) => e == "#").length;
+    expect(onLights, 4);
+  });
+
+  test("1 step corners on", () {
+    final result = step(initial, keepCornersOn: true);
+    result.debugPrint();
+
+    final onLights = result.expand((e) => e).where((e) => e == "#").length;
+    expect(onLights, 18);
+  });
+
+  test("4 steps corners on", () {
+    final result = step(
+      step(
+        step(
+          step(
+            initial,
+            keepCornersOn: true,
+          ),
+          keepCornersOn: true,
+        ),
+        keepCornersOn: true,
+      ),
+      keepCornersOn: true,
+    );
+    result.debugPrint();
+
+    final onLights = result.expand((e) => e).where((e) => e == "#").length;
+    expect(onLights, 14);
+  });
+
+  test("5 steps corners on", () {
+    final result = step(
+      step(
+        step(
+          step(
+            step(
+              initial,
+              keepCornersOn: true,
+            ),
+            keepCornersOn: true,
+          ),
+          keepCornersOn: true,
+        ),
+        keepCornersOn: true,
+      ),
+      keepCornersOn: true,
+    );
+    result.debugPrint();
+
+    final onLights = result.expand((e) => e).where((e) => e == "#").length;
+    expect(onLights, 17);
+  });
+}
+
+extension PrintIt on List<List<String>> {
+  void debugPrint() {
+    print(map((e) => e.join("")).join("\n"));
+  }
+}
