5c6a9455b0051d0bdaf23049f7c88ffacb427780
Julian König <julian.koenig@js-soft.com>
2024-12-01 22:10:46 +0100

    chore: remove strange script

--- _archive/generate.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:io';
-
-void main(List<String> args) {
-  for (int i = 1; i <= 25; i++) {
-    final input = File('input/$i.txt');
-    if (!input.existsSync()) input.writeAsString('');
-  }
-}
